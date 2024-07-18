7e6e0b313d96db963be26162cbf486af17ebcfbe
raimu0210 <raipoke0210@gmaill.com>
2024-07-18 19:34:22 +0900

    search画面の作成

---
+++ src/js/search.js
@@ -0,0 +1,5 @@
+function updateOutput() {
+    var rangeInput = document.getElementById('range-example');
+    var output = document.getElementById('output-value');
+    output.value = rangeInput.value + "円以内";
+}
