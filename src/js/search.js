d300e92ca4c7ffdbfb5f9b22d338d98b84b2fbc1
raimu0210 <raipoke0210@gmaill.com>
2024-07-20 18:54:12 +0900

    search画面 機能追加

--- src/js/search.js
+++ src/js/search.js
@@ -3,3 +3,9 @@ function updateOutput() {
     var output = document.getElementById('output-value');
     output.value = rangeInput.value + "円以内";
 }
+
+function distanceOutput() {
+    var rangeInput = document.getElementById('range-distance');
+    var output = document.getElementById('output-distance');
+    output.value = rangeInput.value > 0 ? rangeInput.value + "m以内" : "指定しない";
+}
